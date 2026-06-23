8c60a6dcbc27b0b548c6c87d1485269fb26d230f
Leonardo Iván Guzmán Zamudio <64369770+LeonardoIGZ@users.noreply.github.com>
2026-06-22 20:10:49 -0600

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        repeated_items = {}
+
+        for char_s, char_t in zip(s, t):
+            repeated_items[char_s] = repeated_items.get(char_s, 0) + 1            
+                          
+            if not repeated_items[char_s]:
+                repeated_items.pop(char_s)
+
+            repeated_items[char_t] = repeated_items.get(char_t, 0) - 1
+                
+            if not repeated_items[char_t]:
+                repeated_items.pop(char_t)
+
+        return not repeated_items
