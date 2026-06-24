440f10cac5873c930391ce5d8eeae23c3da2d0a7
Leonardo Iván Guzmán Zamudio <64369770+LeonardoIGZ@users.noreply.github.com>
2026-06-23 20:06:59 -0600

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        pairs = {}
+
+        for i, num in enumerate(nums):
+            exists = pairs.get(num)
+              
+            if exists is not None:
+                return [exists, i]
+            else:
+                value = target - num
+                pairs[value] = i
+
+        # return None, this will never be reached beacuse the problem guarantee always a pair
+        return [0]
